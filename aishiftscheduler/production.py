51bd4d6e18f36b7f11e14e52786cc4ca06589afa
Klaus Broadstreet <klaus@uts.local>
2024-05-20 16:44:04 +0000

    Revert "after CORS and requirements.txt"
    This reverts commit 2484acd77cacb1701c49b555bda6a0b49a41cc50.

--- aishiftscheduler/production.py
+++ aishiftscheduler/production.py
@@ -297,10 +297,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    # allow_credentials=False,
-    allow_credentials=True,
-    # allow_methods=["GET", "POST", "PUT", "DELETE"],
-    allow_methods=["*"],
+    allow_credentials=False,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
 )
 
